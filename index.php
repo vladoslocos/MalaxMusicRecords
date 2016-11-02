28d6034518e7e46b572eaf43e285989180e8bf2f
vladoslocos <ignjatovic.vladimir93@gmail.com>
2016-11-02 18:50:31 +0100

    Commit #2 -- Ajout Feuille de style "style.css"
    Ajout de la feuille de style.
    Prochaine étape-> Enlever tout le style des pages php et coder le style
    des pages seulement dans la feuille de style.

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+
+		<link rel="stylesheet" href="style.css">
+
+	</head>
+
+	<body>
+
+		<div style="width:80%; background-color:grey;margin:auto;display:block;">
+
+			<div style="width:80%; height:150px;margin:auto;">
+
+				<img src="logo.jpg" style=" width: 150px;float:left;"/>
+				<div style="font-size:90px;font-style:italic;float:left;">Malax Musique Records</div>
+
+			</div>
+
+			<nav>
+
+				<a href="index.php">Accueil</a>
+				<a href="actualites.php">Actualités MMR</a>
+				<a href="punchline.php">Punchline</a>
+				<a href="messagerie.php">Messagerie</a>
+
+			</nav>
+
+			<br/>
+			<br/>
+
+			<div style="width:80%; height:600px;margin:auto;">
+				<h1 style="text-align:center;font-size:60px;font-family:Rockwell;"> MMR </h1>
+				<p style="text-align:center;"> Ce site a été mis en place dans le but de réunir tout l'ensemble de la culture RAP, de l'ancien au nouveau style. </p>
+				<p> Suivez l'actualités du RAP français en live, commentez les nouveaux sons, partager les votres et enfin balancer vos punchline.</p>
+				<p> Vous avez la chance de démontrer la rage et le pouvoir de votre plume à travers ce site </p>
+				<p> Pour bénificier de toute les fonctionnalités, rejoignez la communauté en vous inscrivant </p>
+			</div>
+		
+		</div>
+
+		<footer>
+			
+			<p class="text-center">-- ÉQUIPE MMR --</p>
+		
+		</footer>
+	
+	</body>
+
+</html>
---
+++ punchline.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<link rel="stylesheet" href="style.css">
+		
+	</head>
+
+	<body>
+
+		<div style="width:80%; background-color:grey;margin:auto;display:block;">
+
+			<div style="width:80%; height:150px;margin:auto;">
+
+				<img src="logo.jpg" style=" width: 150px;float:left;"/>
+				<div style="font-size:90px;font-style:italic;float:left;">Malax Musique Records</div>
+
+			</div>
+
+			<nav>
+
+				<a href="index.php">Accueil</a>
+				<a href="actualites.php">Actualités MMR</a>
+				<a href="punchline.php">Punchline</a>
+				<a href="messagerie.php">Messagerie</a>
+
+			</nav>
+
+
+			<br/>
+			<br/>
+
+			<div style="width:80%; height:150px;margin:auto;">
+				<h1 style="text-align:center;font-size:60px;font-family:Rockwell;"> PUNCHLINE </h1>
+				<p style="text-align:center;"> Des changements sont en cours de réalisations... Revenez dans quelques jours. </p>
+			</div>
+		
+		</div>
+
+		<footer class="text-center">
+			
+			<p>-- ÉQUIPE MMR --</p>
+		
+		</footer>
+	
+	</body>
+
+</html>
